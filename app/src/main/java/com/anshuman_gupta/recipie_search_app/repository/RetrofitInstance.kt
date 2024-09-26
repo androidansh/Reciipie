2099c10921bb767738ade348e3171629ee2123dc
Anshuman Gupta <162359571+androidansh@users.noreply.github.com>
2024-09-26 22:23:45 +0530

    Update RetrofitInstance.kt

--- app/src/main/java/com/anshuman_gupta/recipie_search_app/repository/RetrofitInstance.kt
+++ app/src/main/java/com/anshuman_gupta/recipie_search_app/repository/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
     private val BASE_URL = "https://api.spoonacular.com/recipes/"
-    val API_KEY = "23c1414d4c6946198d679d79459d4990"
+    val API_KEY = "Add Your Own Api Key"
 
     val retrofitInstance by lazy {
         Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
